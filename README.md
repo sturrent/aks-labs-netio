5fc13c290b10c3a1b06dd9f4ec047edb5a1841bb
Sergio Turrent <romeo.turrent@gmail.com>
2022-05-30 14:55:00 -0600

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# aks-labs-netio
-aks-labs-netio
+# s
+This is a set of scripts and tools use to generate a docker image that will have the aks-labs-netio binary used to evaluate your AKS troubleshooting skills.
+
+It uses the shc_script_converter.sh (build using the following tool https://github.com/neurobin/shc) to abstract the lab scripts on binary format and then the use the Dockerfile to pack everyting on a Ubuntu container with az cli and kubectl.
+
+Any time the labs script require an update the github actions can be use to trigger a new build and push of the updated image. This will take care of building a new script binary as well as new docker image that will get pushed to the corresponding registry. The actions will get triggered any time a new release gets published.
+
+Here is the general usage for the image and aks-labs-netio tool:
+
+Run in docker: `docker run -it sturrent/aks-labs-netio:latest`
+
+aks-labs-netio tool usage:
+```
+$ aks-labs-netio -h
+aks-labs-netio usage: aks-labs-netio -l <LAB#> -u <USER_ALIAS> [-v|--validate] [-r|--region] [-h|--help] [--version]
+
+
+Here is the list of current labs available:
+
+*************************************************************************************
+*        1. Website hosted on AKS not reachable over public IP
+*        2. Firewall issue
+*        3. Netpol issue
+*************************************************************************************
+
+"-l|--lab" Lab scenario to deploy (3 possible options)
+"-r|--region" region to create the resources
+"--version" print version of aks-labs-netio
+"-h|--help" help info
+```
---
+++ akslabs_scripts/aks-labs-netio.sh
@@ -0,0 +1,510 @@
+#!/bin/bash
+
+# script name: aks-labs-netio.sh
+# Version v0.0.1 20220530
+# Set of tools to deploy AKS troubleshooting labs
+
+# "-l|--lab" Lab scenario to deploy
+# "-r|--region" region to deploy the resources
+# "-u|--user" User alias to add on the lab name
+# "-h|--help" help info
+# "--version" print version
+
+# read the options
+TEMP=`getopt -o g:n:l:r:u:hv --long resource-group:,name:,lab:,region:,user:,help,validate,version -n 'aks-labs-netio.sh' -- "$@"`
+eval set -- "$TEMP"
+
+# set an initial value for the flags
+RESOURCE_GROUP=""
+CLUSTER_NAME=""
+LAB_SCENARIO=""
+USER_ALIAS=""
+LOCATION="eastus2"
+VALIDATE=0
+HELP=0
+VERSION=0
+
+while true ;
+do
+    case "$1" in
+        -h|--help) HELP=1; shift;;
+        -g|--resource-group) case "$2" in
+            "") shift 2;;
+            *) RESOURCE_GROUP="$2"; shift 2;;
+            esac;;
+        -n|--name) case "$2" in
+            "") shift 2;;
+            *) CLUSTER_NAME="$2"; shift 2;;
+            esac;;
+        -l|--lab) case "$2" in
+            "") shift 2;;
+            *) LAB_SCENARIO="$2"; shift 2;;
+            esac;;
+        -r|--region) case "$2" in
+            "") shift 2;;
+            *) LOCATION="$2"; shift 2;;
+            esac;;
+        -u|--user) case "$2" in
+            "") shift 2;;
+            *) USER_ALIAS="$2"; shift 2;;
+            esac;;    
+        -v|--validate) VALIDATE=1; shift;;
+        --version) VERSION=1; shift;;
+        --) shift ; break ;;
+        *) echo -e "Error: invalid argument\n" ; exit 3 ;;
+    esac
+done
+
+# Variable definition
+SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
+SCRIPT_NAME="$(echo $0 | sed 's|\.\/||g')"
+SCRIPT_VERSION="Version v0.0.1 20220530"
+
+# Funtion definition
+
+# az login check
+function az_login_check () {
+    if $(az account list 2>&1 | grep -q 'az login')
+    then
+        echo -e "\n--> Warning: You have to login first with the 'az login' command before you can run this lab tool\n"
+        az login -o table
+    fi
+}
+
+# check resource group and cluster
+function check_resourcegroup_cluster () {
+    RESOURCE_GROUP="$1"
+    CLUSTER_NAME="$2"
+
+    RG_EXIST=$(az group show -g $RESOURCE_GROUP &>/dev/null; echo $?)
+    if [ $RG_EXIST -ne 0 ]
+    then
+        echo -e "\n--> Creating resource group ${RESOURCE_GROUP}...\n"
+        az group create --name $RESOURCE_GROUP --location $LOCATION -o table &>/dev/null
+    else
+        echo -e "\nResource group $RESOURCE_GROUP already exists...\n"
+    fi
+
+    CLUSTER_EXIST=$(az aks show -g $RESOURCE_GROUP -n $CLUSTER_NAME &>/dev/null; echo $?)
+    if [ $CLUSTER_EXIST -eq 0 ]
+    then
+        echo -e "\n--> Cluster $CLUSTER_NAME already exists...\n"
+        echo -e "Please remove that one before you can proceed with the lab.\n"
+        exit 5
+    fi
+}
+
+# validate cluster exists
+function validate_cluster_exists () {
+    RESOURCE_GROUP="$1"
+    CLUSTER_NAME="$2"
+
+    CLUSTER_EXIST=$(az aks show -g $RESOURCE_GROUP -n $CLUSTER_NAME &>/dev/null; echo $?)
+    if [ $CLUSTER_EXIST -ne 0 ]
+    then
+        echo -e "\n--> ERROR: Failed to create cluster $CLUSTER_NAME in resource group $RESOURCE_GROUP ...\n"
+        exit 5
+    fi
+}
+
+# Usage text
+function print_usage_text () {
+    NAME_EXEC="aks-labs-netio"
+    echo -e "$NAME_EXEC usage: $NAME_EXEC -l <LAB#> -u <USER_ALIAS> [-v|--validate] [-r|--region] [-h|--help] [--version]\n"
+    echo -e "\nHere is the list of current labs available:\n
+*************************************************************************************
+*\t 1. Website hosted on AKS not reachable over public IP
+*\t 2. Firewall issue
+*\t 3. Netpol issue
+*************************************************************************************\n"
+}
+
+# Lab scenario 1
+function lab_scenario_1 () {
+    CLUSTER_NAME=aks-labs-netio-ex1-${USER_ALIAS}
+    RESOURCE_GROUP=aks-labs-netio-ex1-rg-${USER_ALIAS}
+    check_resourcegroup_cluster $RESOURCE_GROUP $CLUSTER_NAME
+    VNET_NAME=aks-vnet-ex1
+    SUBNET_NAME=aks-subnet-ex1
+    NSG_NAME=sec-rules
+
+    echo -e "\n--> Deploying cluster for lab${LAB_SCENARIO}...\n"
+    az network vnet create \
+    --resource-group $RESOURCE_GROUP \
+    --name $VNET_NAME \
+    --address-prefixes 10.100.60.0/22 \
+    --subnet-name $SUBNET_NAME \
+    --subnet-prefix 10.100.60.0/22 \
+    -o table
+	
+    SUBNET_ID=$(az network vnet subnet list \
+    --resource-group $RESOURCE_GROUP \
+    --vnet-name $VNET_NAME \
+    --query [].id --output tsv)
+
+    az network nsg rule create -g $RESOURCE_GROUP --nsg-name $NSG_NAME \
+    -n SecRule1  --priority 200 \
+    --source-address-prefixes VirtualNetwork \
+    --destination-address-prefixes Internet \
+    --destination-port-ranges '*' \
+    --direction Inbound \
+    --access Deny \
+    --protocol '*' \
+    --description "Security test" &>/dev/null
+
+    az network vnet subnet update \
+    --resource-group $RESOURCE_GROUP \
+    --vnet-name $VNET_NAME \
+    --name $SUBNET_NAME \
+    --network-security-group $NSG_NAME
+
+    az aks create \
+    --resource-group $RESOURCE_GROUP \
+    --name $CLUSTER_NAME \
+    --location $LOCATION \
+    --node-count 2 \
+    --network-plugin azure \
+    --service-cidr 10.0.0.0/16 \
+    --dns-service-ip 10.0.0.10 \
+    --docker-bridge-address 172.17.0.1/16 \
+    --vnet-subnet-id $SUBNET_ID \
+    --tag aks-net-lab=${LAB_SCENARIO} \
+    --generate-ssh-keys \
+    --yes \
+    -o table
+
+    validate_cluster_exists $RESOURCE_GROUP $CLUSTER_NAME
+
+    az aks get-credentials -g $RESOURCE_GROUP -n $CLUSTER_NAME --overwrite-existing &>/dev/null
+
+cat <<EOF | kubectl apply -f -
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: aks-helloworld-one
+spec:
+  replicas: 1
+  selector:
+    matchLabels:
+      app: aks-helloworld-one
+  template:
+    metadata:
+      labels:
+        app: aks-helloworld-one
+    spec:
+      containers:
+      - name: aks-helloworld-one
+        image: mcr.microsoft.com/azuredocs/aks-helloworld:v1
+        ports:
+        - containerPort: 80
+        env:
+        - name: TITLE
+          value: "Welcome to Azure Kubernetes Service (AKS)"
+---
+apiVersion: v1
+kind: Service
+metadata:
+  name: aks-helloworld-one-pub
+spec:
+  type: LoadBalancer
+  ports:
+  - port: 80
+  selector:
+    app: aks-helloworld-one
+EOF
+
+    echo -e "\n\n--> Please wait while we are preparing the environment for you to troubleshoot...\n"
+    sleep 60
+    CLUSTER_URI="$(az aks show -g $RESOURCE_GROUP -n $CLUSTER_NAME --query id -o tsv)"
+    WEB_IP="$(kubectl get svc aks-helloworld-one-pub | grep -v '^NAME' | awk '{print $4}')"
+    echo -e "\n************************************************************************\n"
+    echo -e "\n--> Issue description: \n Website hosted on AKS cluster with public IP $WEB_IP is not reachable...\n"
+    echo -e "Cluster uri == ${CLUSTER_URI}\n"
+}
+
+function lab_scenario_1_validation () {
+    CLUSTER_NAME=aks-labs-netio-ex1-${USER_ALIAS}
+    RESOURCE_GROUP=aks-labs-netio-ex1-rg-${USER_ALIAS}
+    VNET_NAME=aks-vnet-ex1
+    SUBNET_NAME=aks-subnet-ex1
+    LAB_TAG="$(az aks show -g $RESOURCE_GROUP -n $CLUSTER_NAME --query tags -o yaml 2>/dev/null | grep aks-net-lab | cut -d ' ' -f2 | tr -d "'")"
+    echo -e "\n+++++++++++++++++++++++++++++++++++++++++++++++++++++"
+    echo -e "--> Running validation for Lab scenario $LAB_SCENARIO\n"
+    if [ -z $LAB_TAG ]
+    then
+        echo -e "\n--> Error: Cluster $CLUSTER_NAME in resource group $RESOURCE_GROUP was not created with this tool for lab $LAB_SCENARIO and cannot be validated...\n"
+        exit 6
+    elif [ $LAB_TAG -eq $LAB_SCENARIO ]
+    then
+        az aks get-credentials -g $RESOURCE_GROUP -n $CLUSTER_NAME --overwrite-existing &>/dev/null
+        CLUSTER_RESOURCE_GROUP=$(az aks show -g $RESOURCE_GROUP -n $CLUSTER_NAME --query nodeResourceGroup -o tsv)
+        WEB_STATUS="$(curl -m 5 -s -o /dev/null -w "%{http_code}" http://$WEB_IP)"
+        if [ "$WEB_STATUS" -eq "200" ]
+        then
+            echo -e "\n\n========================================================"
+            echo -e "\nThe website hosted on $WEB_IP is reachable\n"
+        else
+            echo -e "\nScenario $LAB_SCENARIO is still FAILED\n"
+        fi
+    else
+        echo -e "\n--> Error: Cluster $CLUSTER_NAME in resource group $RESOURCE_GROUP was not created with this tool for lab $LAB_SCENARIO and cannot be validated...\n"
+        exit 6
+    fi
+}
+
+# Lab scenario 2
+function lab_scenario_2 () {
+    CLUSTER_NAME=aks-labs-netio-ex2-${USER_ALIAS}
+    RESOURCE_GROUP=aks-labs-netio-ex2-rg-${USER_ALIAS}
+    check_resourcegroup_cluster $RESOURCE_GROUP $CLUSTER_NAME
+    VNET_NAME=aks-vnet-ex2
+    SUBNET_NAME=aks-subnet-ex2
+    UDR_NAME=security-routes
+    check_resourcegroup_cluster $RESOURCE_GROUP $CLUSTER_NAME
+
+    az network vnet create \
+    --resource-group $RESOURCE_GROUP \
+    --name $VNET_NAME \
+    --address-prefixes 192.168.0.0/16 \
+    --subnet-name $SUBNET_NAME \
+    --subnet-prefix 192.168.100.0/24 \
+    -o table
+	
+    SUBNET_ID=$(az network vnet subnet list \
+    --resource-group $RESOURCE_GROUP \
+    --vnet-name $VNET_NAME \
+    --query [].id --output tsv)
+
+    az network route-table create -g $RESOURCE_GROUP --name $UDR_NAME -o table
+    az network route-table route create -g $RESOURCE_GROUP --route-table-name $UDR_NAME -n main-route \
+    --next-hop-type VirtualAppliance --address-prefix 0.0.0.0/0 --next-hop-ip-address 10.0.0.1 -o table &>/dev/null
+    az network vnet subnet update -g $RESOURCE_GROUP -n $SUBNET_NAME --vnet-name $VNET_NAME --route-table $UDR_NAME -o table
+
+    echo -e "\n--> Deploying cluster for lab${LAB_SCENARIO}...\n"
+    az aks create \
+    --resource-group $RESOURCE_GROUP \
+    --name $CLUSTER_NAME \
+    --location $LOCATION \
+    --node-count 1 \
+    --network-plugin azure \
+    --service-cidr 10.0.0.0/16 \
+    --dns-service-ip 10.0.0.10 \
+    --docker-bridge-address 172.17.0.1/16 \
+    --vnet-subnet-id $SUBNET_ID \
+    --generate-ssh-keys \
+    --tag aks-net-lab=${LAB_SCENARIO} \
+	--yes \
+    -o table
+
+    validate_cluster_exists $RESOURCE_GROUP $CLUSTER_NAME
+    
+    MC_RESOURCE_GROUP=$(az aks show -g $RESOURCE_GROUP -n $CLUSTER_NAME --query nodeResourceGroup -o tsv)
+    CLUSTER_URI="$(az aks show -g $RESOURCE_GROUP -n $CLUSTER_NAME --query id -o tsv)"
+    echo -e "\n\n********************************************************"
+    echo -e "\n--> Issue description: \nNew cluster deployment fails with \"vmssCSE failed: connect to mcr.microsoft.com port 443 (tcp) failed: Connection timed out\" \nAnd there are no nodes in the cluster from Kubernetes perspective\n"
+    echo -e "Cluster uri == ${CLUSTER_URI}\n"
+}
+
+function lab_scenario_2_validation () {
+    CLUSTER_NAME=aks-labs-netio-ex2-${USER_ALIAS}
+    RESOURCE_GROUP=aks-labs-netio-ex2-rg-${USER_ALIAS}
+    VNET_NAME=aks-vnet-ex2
+    SUBNET_NAME=aks-subnet-ex2
+    LAB_TAG="$(az aks show -g $RESOURCE_GROUP -n $CLUSTER_NAME --query tags -o yaml 2>/dev/null | grep aks-net-lab | cut -d ' ' -f2 | tr -d "'")"
+    echo -e "\n+++++++++++++++++++++++++++++++++++++++++++++++++++++"
+    echo -e "--> Running validation for Lab scenario $LAB_SCENARIO\n"
+    if [ -z $LAB_TAG ]
+    then
+        echo -e "\n--> Error: Cluster $CLUSTER_NAME in resource group $RESOURCE_GROUP was not created with this tool for lab $LAB_SCENARIO and cannot be validated...\n"
+        exit 6
+    elif [ $LAB_TAG -eq $LAB_SCENARIO ]
+    then
+        az aks get-credentials -g $RESOURCE_GROUP -n $CLUSTER_NAME --overwrite-existing &>/dev/null
+        CLUSTER_RESOURCE_GROUP=$(az aks show -g $RESOURCE_GROUP -n $CLUSTER_NAME --query nodeResourceGroup -o tsv)
+        GET_NODES="$(kubectl get no 2>&1)"
+        if [ "$GET_NODES" != "No resources found" ]
+        then
+            echo -e "\n\n========================================================"
+            echo -e "\nThe cluster nodes outbound looks good now\n"
+        else
+            echo -e "\nScenario $LAB_SCENARIO is still FAILED\n"
+        fi
+    else
+        echo -e "\n--> Error: Cluster $CLUSTER_NAME in resource group $RESOURCE_GROUP was not created with this tool for lab $LAB_SCENARIO and cannot be validated...\n"
+        exit 6
+    fi
+}
+
+# Lab scenario 3
+function lab_scenario_3 () {
+    CLUSTER_NAME=aks-labs-netio-ex3-${USER_ALIAS}
+    RESOURCE_GROUP=aks-labs-netio-ex3-rg-${USER_ALIAS}
+    check_resourcegroup_cluster $RESOURCE_GROUP $CLUSTER_NAME
+    
+    echo -e "\n--> Deploying cluster for lab${LAB_SCENARIO}...\n"
+    az aks create \
+    --resource-group $RESOURCE_GROUP \
+    --name $CLUSTER_NAME \
+    --location $LOCATION \
+    --node-count 1 \
+    --generate-ssh-keys \
+    --tag aks-net-lab=${LAB_SCENARIO} \
+	--yes \
+    -o table
+
+    validate_cluster_exists $RESOURCE_GROUP $CLUSTER_NAME
+
+    echo -e "\n\n--> Please wait while we are preparing the environment for you to troubleshoot...\n"
+    az aks get-credentials -g $RESOURCE_GROUP -n $CLUSTER_NAME --overwrite-existing &>/dev/null
+
+cat <<EOF | kubectl apply -f -
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: aks-helloworld-one  
+spec:
+  replicas: 1
+  selector:
+    matchLabels:
+      app: aks-helloworld-one
+  template:
+    metadata:
+      labels:
+        app: aks-helloworld-one
+    spec:
+      containers:
+      - name: aks-helloworld-one
+        image: mcr.microsoft.com/azuredocs/aks-helloworld:v1
+        ports:
+        - containerPort: 88
+        env:
+        - name: TITLE
+          value: "Welcome to Azure Kubernetes Service (AKS)"
+---
+apiVersion: v1
+kind: Service
+metadata:
+  name: aks-helloworld-one  
+spec:
+  type: LoadBalancer
+  ports:
+  - port: 80
+    targetPort: 88
+  selector:
+    app: aks-helloworld-one
+EOF
+
+    CLUSTER_URI="$(az aks show -g $RESOURCE_GROUP -n $CLUSTER_NAME --query id -o tsv)"
+    echo -e "\n\n********************************************************"
+    echo -e "\n--> Issue description: \nCluster has a web application \"aks-helloworld-one\" exposed with service type LoadBalancer that is currently not reachable...\n"
+    echo -e "\nCluster uri == ${CLUSTER_URI}\n"
+}
+
+function lab_scenario_3_validation () {
+    CLUSTER_NAME=aks-labs-netio-ex3-${USER_ALIAS}
+    RESOURCE_GROUP=aks-labs-netio-ex3-rg-${USER_ALIAS}
+    
+    LAB_TAG="$(az aks show -g $RESOURCE_GROUP -n $CLUSTER_NAME --query tags -o yaml 2>/dev/null | grep aks-net-lab | cut -d ' ' -f2 | tr -d "'")"
+    echo -e "\n+++++++++++++++++++++++++++++++++++++++++++++++++++"
+    echo -e "--> Running validation for Lab scenario $LAB_SCENARIO\n"
+    if [ -z $LAB_TAG ]
+    then
+        echo -e "\n--> Error: Cluster $CLUSTER_NAME in resource group $RESOURCE_GROUP was not created with this tool for lab $LAB_SCENARIO and cannot be validated...\n"
+        exit 6
+    elif [ $LAB_TAG -eq $LAB_SCENARIO ]
+    then
+        az aks get-credentials -g $RESOURCE_GROUP -n $CLUSTER_NAME --overwrite-existing &>/dev/null
+        CLUSTER_RESOURCE_GROUP=$(az aks show -g $RESOURCE_GROUP -n $CLUSTER_NAME --query nodeResourceGroup -o tsv)
+        PUBLIC_IP="$(kubectl get svc aks-helloworld-one | grep -v ^NAME | awk '{print $4}')"
+        SITE_STATUS="$(curl -IL -m 5 $PUBLIC_IP 2>/dev/null | grep ^HTTP | awk '{print $2}')"
+        if [ "$SITE_STATUS" == '200' ]
+        then
+            echo -e "\n\n========================================================"
+            echo -e "\nThe webservice looks good now\n"
+        else
+            echo -e "\nScenario $LAB_SCENARIO is still FAILED\n"
+        fi
+    else
+        echo -e "\n--> Error: Cluster $CLUSTER_NAME in resource group $RESOURCE_GROUP was not created with this tool for lab $LAB_SCENARIO and cannot be validated...\n"
+        exit 6
+    fi
+}
+
+#if -h | --help option is selected usage will be displayed
+if [ $HELP -eq 1 ]
+then
+	print_usage_text
+    echo -e '"-l|--lab" Lab scenario to deploy (3 possible options)
+"-r|--region" region to create the resources
+"--version" print version of aks-labs-netio
+"-h|--help" help info\n'
+	exit 0
+fi
+
+if [ $VERSION -eq 1 ]
+then
+	echo -e "$SCRIPT_VERSION\n"
+	exit 0
+fi
+
+if [ -z $LAB_SCENARIO ]; then
+	echo -e "\n--> Error: Lab scenario value must be provided. \n"
+	print_usage_text
+	exit 9
+fi
+
+if [ -z $USER_ALIAS ]; then
+	echo -e "Error: User alias value must be provided. \n"
+	print_usage_text
+	exit 10
+fi
+
+# lab scenario has a valid option
+if [[ ! $LAB_SCENARIO =~ ^[1-3]+$ ]];
+then
+    echo -e "\n--> Error: invalid value for lab scenario '-l $LAB_SCENARIO'\nIt must be value from 1 to 3\n"
+    exit 11
+fi
+
+# main
+echo -e "\n--> AKS Troubleshooting sessions
+********************************************
+
+This tool will use your default subscription to deploy the lab environments.
+Verifing if you are authenticated already...\n"
+
+# Verify az cli has been authenticated
+az_login_check
+
+if [ $LAB_SCENARIO -eq 1 ] && [ $VALIDATE -eq 0 ]
+then
+    check_resourcegroup_cluster
+    lab_scenario_1
+
+elif [ $LAB_SCENARIO -eq 1 ] && [ $VALIDATE -eq 1 ]
+then
+    lab_scenario_1_validation
+
+elif [ $LAB_SCENARIO -eq 2 ] && [ $VALIDATE -eq 0 ]
+then
+    check_resourcegroup_cluster
+    lab_scenario_2
+
+elif [ $LAB_SCENARIO -eq 2 ] && [ $VALIDATE -eq 1 ]
+then
+    lab_scenario_2_validation
+
+elif [ $LAB_SCENARIO -eq 3 ] && [ $VALIDATE -eq 0 ]
+then
+    check_resourcegroup_cluster
+    lab_scenario_3
+
+elif [ $LAB_SCENARIO -eq 3 ] && [ $VALIDATE -eq 1 ]
+then
+    lab_scenario_3_validation
+else
+    echo -e "\n--> Error: no valid option provided\n"
+    exit 12
+fi
+
+exit 0
